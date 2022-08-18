8c910b6eba0f62d019fb92b1450ee5484bc8ded8
superjojo140 <github@superjojo.de>
2022-08-18 12:30:00 +0200

    Add telephone_notes module

---
+++ modules/telephone_notes/telephone_notes.js
@@ -0,0 +1,49 @@
+//*********************************************Telefonnotizen*************************************
+refreshTelephoneRecipientsDropdown();
+$('[data-toggle="tooltip"]').tooltip();
+
+async function refreshTelephoneRecipientsDropdown() {
+    const userList = await stubegru.modules.userUtils.getUserByPermission("telefonnotiz");
+    $("#telephonenote_recipients").html("<option value=''>Bitte wählen...</option>"); //Clear old recipients
+    for (const user of userList) {
+        $("#telephonenote_recipients").append($('<option>', {
+            value: user.mail,
+            text: user.name
+        }));
+    }
+
+}
+
+function sendTelephoneNote() { 
+    let note = {};
+    note.name = $("#telephonenote_name").val();
+    note.mnumber = $("#telephonenote_mnumber").val();
+    note.phone = $("#telephonenote_phone").val();
+    note.mail = $("#telephonenote_mail").val();
+    note.note = $("#telephonenote_note").val();
+    note.recipient = $("#telephonenote_recipients").val();
+    note.issue = $("#telephonenote_issue").val();
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        data: note,
+        url: `${stubegru.constants.BASE_URL}/modules/telephone_notes/send_telephone_note.php`,
+        success: function (data) {
+            stubegru.modules.alerts.alert({
+                title: "Telefonnotiz senden",
+                text: data.message,
+                type: data.status
+            });
+            if (data.status == "success") { document.getElementById("telephonenote_form").reset(); }
+        },
+        error: function (data) {
+            stubegru.modules.alerts.alert({
+                title: "Es ist ein Fehler aufgetreten",
+                text: "Die Telefonnotiz konnte nicht versendet werden",
+                type: "error"
+            });
+        }
+    });
+    
+}
