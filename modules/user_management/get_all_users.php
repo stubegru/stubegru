20c93d66579ddc3432b7c18aea130eee4ba34af0
superjojo140 <github@superjojo.de>
2022-10-06 11:52:42 +0200

    Dont fetch users password hashes

--- modules/user_management/get_all_users.php
+++ modules/user_management/get_all_users.php
@@ -4,13 +4,7 @@ $BASE_PATH = getenv("BASE_PATH");
 require_once "$BASE_PATH/utils/auth_and_database.php";
 permissionRequest("USER_READ");
 
-$toReturn = array();
-$selectStatement = $dbPdo->prepare("SELECT * FROM Nutzer;");
-$selectStatement->execute();
+$selectStatement = $dbPdo->query("SELECT `id`, `name`, `mail`, `account`, `role`, `erfassungsdatum`, `erfasser` FROM `Nutzer`;");
 $resultList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($resultList as $row) {
-    $toReturn[] = $row;
-}
-
-echo json_encode($toReturn);
+echo json_encode($resultLists);
