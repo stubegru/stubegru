3fb9d0de6371d13f30ee58fa9c35ee18c2e203e7
superjojo140 <github@superjojo.de>
2022-10-21 11:27:22 +0200

    Update get_notifications and reduce DB calls #3

--- modules/notifications/get_notifications.php
+++ modules/notifications/get_notifications.php
@@ -1,61 +1,36 @@
 <?php
-
-// Dieses Script prüft, ob es Benachrichtigungen für den Nutzer gibt und stellt sie als JSON zur Verfügung
-
 $BASE_PATH = getenv("BASE_PATH");
 require_once "$BASE_PATH/utils/auth_and_database.php";
 require_once "$BASE_PATH/modules/user_utils/user_utils.php";
 $own_id = $_SESSION['id'];
+$notificationId = $_POST["notificationId"]; //"all" => alle
 
 //[Aufräumen] Lösche alle Notifications, die älter als 2 Monate sind
 require_once "$BASE_PATH/modules/notifications/delete_old_notifications.php";
 
-
-
-if (isset($_POST["notificationId"])){
-    $notificationId = $_POST["notificationId"]; //"all" => alle
-}
-else{
-     echo json_encode(array("status" => "error", "message" => "Bitte eine id oder \"all\" als notificationId uebergeben"));
-    exit;
-}
-
-$toReturn = array();
-
-
 //Prüfen, ob alle oder ein bestimmter Datensatz geholt werden soll
-if ($notificationId == "all"){
+if ($notificationId == "all") {
     //hohle alle Benachrichtigungen
     $selectStatement = $dbPdo->prepare("SELECT notifications . * , notification_user.read FROM notifications, notification_user WHERE notifications.id = notification_user.notificationId AND notification_user.userId = :ownId ORDER BY `timestamp` DESC;");
     $selectStatement->bindValue(':ownId', $own_id);
     $selectStatement->execute();
     $resultList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
-}
-else if (is_numeric($notificationId)){
+} else if (is_numeric($notificationId)) {
     //hohle eine spezielle Benachrichtigung
     $selectStatement = $dbPdo->prepare("SELECT notifications.* , notification_user.read FROM notifications, notification_user WHERE notifications.id=:notificationId AND notification_user.userId = :ownId LIMIT 1;");
     $selectStatement->bindValue(':ownId', $own_id);
     $selectStatement->bindValue(':notificationId', $notificationId);
     $selectStatement->execute();
     $resultList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
-}
-else{
+} else {
     echo json_encode(array("status" => "error", "message" => "Bitte eine id oder \"all\" als absenceId uebergeben"));
     exit;
 }
 
-//Ungelesene Benachrichtignugnen abarbeiten
-foreach ($resultList as $row) {
-    //Informationen aus Benachrichtigungen Tabelle holen
-    $row["notificationId"]=$row["id"];
-    $row["readState"]=$row["read"];
-   
-    //Name des Autors aus Nutzer Tabelle holen
-    $row["userName"]=getUserAttribute($row["userId"], "name");
-    
-    //In Rückgabearray einfügen
-    $toReturn[] = $row;    
+$allUsers = getUserList(); //from user_utils.php
+foreach ($resultList as &$row) {
+    $row["userName"] = $allUsers[$row["userId"]]["name"];
+    $row["read"] = $row["read"] ? true : false;
 }
 
-
-echo json_encode($toReturn);
+echo json_encode($resultList);
--- modules/user_utils/user_utils.php
+++ modules/user_utils/user_utils.php
@@ -1,9 +1,10 @@
 <?php
-
-
+$BASE_PATH = getenv("BASE_PATH");
+require_once "$BASE_PATH/utils/auth_and_database.php";
 
 function getUserAttribute($userId, $attribute)
 {
+    permissionRequest("USER_READ");
     global $dbPdo;
 
     $selectStatement = $dbPdo->prepare("SELECT `$attribute` FROM `Nutzer` WHERE `ID`=:userId;");
@@ -16,4 +17,20 @@ function getUserAttribute($userId, $attribute)
     } else {
         return null;
     }
+}
+
+function getUserList()
+{
+    permissionRequest("USER_READ");
+    global $dbPdo;
+    $selectStatement = $dbPdo->query("SELECT `id`, `name`, `mail`, `account`, `role`, `erfassungsdatum`, `erfasser` FROM `Nutzer`;");
+    $resultList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+
+    //index by id
+    $toReturn = array();
+    foreach ($resultList as $row) {
+        $toReturn[$row["id"]] = $row;
+    }
+
+    return $toReturn;
 }
