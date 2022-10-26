6b6b4410c21d0a766e15c07d46054ceb6b7fb68e
superjojo140 <github@superjojo.de>
2022-10-26 12:29:04 +0200

    use postRenderHooks in notifications #54

--- modules/notifications/notifications.js
+++ modules/notifications/notifications.js
@@ -2,34 +2,37 @@
 stubegru.modules.notifications = {};
 stubegru.modules.notifications.keepListOpen = false;
 
-//register menu entry for notifications
-stubegru.modules.menubar.addItem("secondary", `<li><a title="Einstellen über welche Ereignisse du informiert wirst." onclick="showNotificationModal()"><i class="fa fa-envelope"></i>&nbsp;Benachrichtigungen konfigurieren</a></li>`, -990);
 
-stubegru.modules.menubar.addItem("primary", `<li class="dropdown" id="notificationDropdown">
+
+function initNotificationList() {
+
+    //register menu entry for notifications
+    stubegru.modules.menubar.addItem("secondary", `<li><a title="Einstellen über welche Ereignisse du informiert wirst." onclick="showNotificationModal()"><i class="fa fa-envelope"></i>&nbsp;Benachrichtigungen konfigurieren</a></li>`, -990);
+
+    stubegru.modules.menubar.addItem("primary", `<li class="dropdown" id="notificationDropdown">
 <a class="dropdown-toggle" href="#" aria-expanded="false" data-toggle="dropdown" id="notificationDropdownNavbarButton" title="Benachrichtigungen">
-    <i class="fa fa-bell fa-fw"></i><span class="badge" id="notificationCountBadge"></span>
+  <i class="fa fa-bell fa-fw"></i><span class="badge" id="notificationCountBadge"></span>
 </a>
 <ul class="dropdown-menu dropdown-messages" id="notificationDropdownList">
-    <li>
-        <div class="text-center notification-headline"><i class="fa fa-bell fa-fw"></i>&nbsp;<b>Benachrichtigungen</b></div>
-        <ul class="notification-list" id="privateMessagesList">
-        </ul>
-    </li>
+  <li>
+      <div class="text-center notification-headline"><i class="fa fa-bell fa-fw"></i>&nbsp;<b>Benachrichtigungen</b></div>
+      <ul class="notification-list" id="privateMessagesList">
+      </ul>
+  </li>
 
-    <li class="divider"></li>
-    <li>
-        <div class="btn-group-full-width btn-group" role="group">
-            <button class="btn btn-half btn-danger" id="notificationDeleteAll">Alle löschen</button>
-            <button class="btn btn-half btn-danger"  id="notificationDeleteRead">Gelesene Löschen</button>
-        </div>
-    </li>
+  <li class="divider"></li>
+  <li>
+      <div class="btn-group-full-width btn-group" role="group">
+          <button class="btn btn-half btn-danger" id="notificationDeleteAll">Alle löschen</button>
+          <button class="btn btn-half btn-danger"  id="notificationDeleteRead">Gelesene Löschen</button>
+      </div>
+  </li>
 </ul>
 </li>`, 100);
 
-function initNotificationList() {
-    $('#notificationDropdown').on("hide.bs.dropdown", function () {
-        return stubegru.modules.notifications.keepListOpen ? false : true;
-    });
+
+
+
     $("#notificationDetailView").on("hidden.bs.modal", function () {
         stubegru.modules.notifications.keepListOpen = false;
     })
@@ -37,6 +40,22 @@ function initNotificationList() {
         stubegru.modules.notifications.keepListOpen = true;
     })
 
+
+
+
+    //refresh notification list after re-rendering menubar
+    stubegru.modules.menubar.registerPostRenderHook("-update notifications-", renderNotificationDropdown, -10);
+    stubegru.modules.menubar.registerPostRenderHook("-update notification dropdown listener-", registerNotificationDropdownListener, 0);
+    //Intervall für regelmäßiges Notification Prüfen aktivieren, alle 5 Minuten
+    setInterval(updateNotifications, 5 * 60 * 1000);
+    updateNotifications();
+}
+initNotificationList();
+
+function registerNotificationDropdownListener() {
+    $('#notificationDropdown').on("hide.bs.dropdown", function () {
+        return stubegru.modules.notifications.keepListOpen ? false : true;
+    });
     $("#notificationDeleteRead").on("click", function (event) {
         event.preventDefault();
         event.stopPropagation();
@@ -47,88 +66,92 @@ function initNotificationList() {
         event.stopPropagation();
         deleteNotification("all")
     })
-
-    updateNotifications();
 }
 
-
-
 function updateNotifications() {
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: `${stubegru.constants.BASE_URL}/modules/notifications/get_notifications.php`,
-        data: { notificationId: "all" },
-        success: function (notificationList) {
-
-            stubegru.modules.notifications.list = notificationList;
-            let unreadCounter = 0;
-
-            //Clear Notifications List
-            $("#privateMessagesList").html("");
-
-            for (let n of notificationList) {
-                if (n.read == false) { unreadCounter++ } //count unread notifications
-                const action = getActionDetails(n.action);
-
-                const notificationHtml = `
-                <li class="notification-item" data-notification-id="${n.id}">
-                    <div class="row">
-                        <div class="col-xs-2">
-                            <div class="notification-side-banner notification-${n.read ? "read" : "unread"}"></div>
-                        </div>
-                        <div class="col-xs-10">
-                            <div class="notification-user-content">
-                                <p class="notification-user-info">
-                                    <span class="label label-default"> ${action.icon} &nbsp; ${n.type.name} ${action.verb} </span>
-                                    <span class="pull-right">${formatTimespan(n.timestamp)}</span>
-                                </p>
-                                <p> ${n.title} </p>
-                                <button class="btn btn-danger notification-list-delete-button" style="display:none" data-notification-id="${n.id}"><i class="fas fa-times"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </li>`;
-                $("#privateMessagesList").append(notificationHtml);
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: `${stubegru.constants.BASE_URL}/modules/notifications/get_notifications.php`,
+            data: { notificationId: "all" },
+            success: function (notificationList) {
+                stubegru.modules.notifications.list = notificationList;
+                renderNotificationDropdown();
             }
+        });
+}
 
-            //Show count of unread notifications in menubar
-            $("#notificationCountBadge").html(unreadCounter);
+function renderNotificationDropdown() {
+    if (!stubegru.modules.notifications.list){
+        return false;
+    }
+    
+    let notificationList = stubegru.modules.notifications.list;
+    let unreadCounter = 0;
 
-            //Eventlistener für Detailview registrieren
-            $(".notification-item").on("click", function (event) {
-                event.preventDefault();
-                event.stopPropagation();
-                const id = $(this).attr("data-notification-id");
-                showNotificationDetailView(id);
-            })
+    //Clear Notifications List
+    $("#privateMessagesList").html("");
 
-            //Eventlistener zum Delete Button einblenden registrieren
-            $(".notification-item").mouseenter(function () {
-                $(this).find(".notification-list-delete-button").fadeIn("fast");
-            });
-            $(".notification-item").mouseleave(function () {
-                $(this).find(".notification-list-delete-button").fadeOut("fast");
-            });
-            $(".notification-list-delete-button").on("click", function (event) {
-                event.preventDefault();
-                event.stopPropagation();
-                let id = $(this).attr("data-notification-id");
-                deleteNotification(id);
-            });
+    for (let n of notificationList) {
+        if (n.read == false) { unreadCounter++ } //count unread notifications
+        const action = getActionDetails(n.action);
 
-            //Toggle des ReadState initialisieren
-            $(".notification-side-banner").click(function (event) {
-                //Click Event stoppen
-                event.preventDefault();
-                event.stopPropagation();
-                //Notification Id auslesen
-                const currentNotificationId = $(this).closest("li").attr("data-notification-id");
-                //ReadState auslesen
-                let newReadState = $(this).hasClass("notification-read") ? "unread" : "read";
-                setNotificationReadState(currentNotificationId, newReadState);
-            });
-        }
+        const notificationHtml = `
+        <li class="notification-item" data-notification-id="${n.id}">
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="notification-side-banner notification-${n.read ? "read" : "unread"}"></div>
+                </div>
+                <div class="col-xs-10">
+                    <div class="notification-user-content">
+                        <p class="notification-user-info">
+                            <span class="label label-default"> ${action.icon} &nbsp; ${n.type.name} ${action.verb} </span>
+                            <span class="pull-right">${formatTimespan(n.timestamp)}</span>
+                        </p>
+                        <p> ${n.title} </p>
+                        <button class="btn btn-danger notification-list-delete-button" style="display:none" data-notification-id="${n.id}"><i class="fas fa-times"></i></button>
+                    </div>
+                </div>
+            </div>
+        </li>`;
+        $("#privateMessagesList").append(notificationHtml);
+    }
+
+    //Show count of unread notifications in menubar
+    $("#notificationCountBadge").html(unreadCounter);
+
+    //Eventlistener für Detailview registrieren
+    $(".notification-item").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const id = $(this).attr("data-notification-id");
+        showNotificationDetailView(id);
+    })
+
+    //Eventlistener zum Delete Button einblenden registrieren
+    $(".notification-item").mouseenter(function () {
+        $(this).find(".notification-list-delete-button").fadeIn("fast");
+    });
+    $(".notification-item").mouseleave(function () {
+        $(this).find(".notification-list-delete-button").fadeOut("fast");
+    });
+    $(".notification-list-delete-button").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let id = $(this).attr("data-notification-id");
+        deleteNotification(id);
+    });
+
+    //Toggle des ReadState initialisieren
+    $(".notification-side-banner").click(function (event) {
+        //Click Event stoppen
+        event.preventDefault();
+        event.stopPropagation();
+        //Notification Id auslesen
+        const currentNotificationId = $(this).closest("li").attr("data-notification-id");
+        //ReadState auslesen
+        let newReadState = $(this).hasClass("notification-read") ? "unread" : "read";
+        setNotificationReadState(currentNotificationId, newReadState);
     });
 }
 
@@ -297,15 +320,6 @@ function showNotificationDetailView(notificationId) {
     setNotificationReadState(notificationId, "read");
 }
 
-
-
-
-
-
-//Intervall für regelmäßiges Notification Prüfen aktivieren, alle 5 Minuten
-setInterval(updateNotifications, 5 * 60 * 1000);
-setTimeout(initNotificationList, 2000); //refresh Notifications, sometimes direct loading failes due to unknown (and magic) reasons
-setTimeout(updateNotifications, 5000); //refresh once again (just to be really safe)
 
 
 /**
