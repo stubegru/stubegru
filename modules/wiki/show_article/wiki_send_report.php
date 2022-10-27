1e3bf421cc5ddb66316ace43fa9d9072d541a9ad
superjojo140 <github@superjojo.de>
2022-10-27 10:55:43 +0200

    Use new notifications for reports #3

--- modules/wiki/show_article/wiki_send_report.php
+++ modules/wiki/show_article/wiki_send_report.php
@@ -7,22 +7,28 @@ require_once "$BASE_PATH/utils/auth_and_database.php";
 permissionRequest("WIKI_READ");
 require_once "$BASE_PATH/modules/user_utils/user_utils.php";
 require_once "$BASE_PATH/modules/notifications/notification_system.php";
-$userId = $_SESSION["id"];
+$own_id = $_SESSION["id"];
 $heute = date("Y-m-d");
 
 $reportReason = $_POST["reportReason"];
 $reportArticleId = $_POST["reportArticleId"];
 $reportNotes = $_POST["reportNotes"];
 
-$selectStatement = $dbPdo->prepare("SELECT * FROM `wiki_artikel` WHERE id = :reportArticleId;");
+//get article heading
+$selectStatement = $dbPdo->prepare("SELECT heading FROM `wiki_artikel` WHERE id = :reportArticleId;");
 $selectStatement->bindValue(':reportArticleId', $reportArticleId);
 $selectStatement->execute();
-$resultList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+$reportArticleName = $selectStatement->fetchColumn();
 
-foreach ($resultList as $row) {
-    $reportArticleName = $row["heading"];
-    newNotification($constants["report"], $reportArticleId, $reportArticleName, $reportNotes, $reportReason, $userId, $constants["info"]);
-}
+//get current users name
+$own_name = getUserAttribute($own_id,"name");
 
+$wikiLink = getenv(("BASE_URL")) . "?view=wiki_show_article&artikel=$reportArticleId";
+$notificationText = "Es wurde ein Problem mit dem Wiki Artikel <b>$reportArticleName</b> gemeldet<br><br>
+<b>Grund der Meldung:</b> $reportReason<br>
+<b>Gemeldet von:</b> $own_name<br>
+<b>Hinweis:</b> $reportNotes<br><br>
+Der entsprechende Wiki Artikel ist hier zu finden:<br><a href='$wikiLink'>$reportArticleName</a>";
+newNotification("WIKI_REPORT", $reportArticleId, $reportReason, $notificationText, $own_id, "INFO");
 
 echo json_encode(array("status" => "success"));
