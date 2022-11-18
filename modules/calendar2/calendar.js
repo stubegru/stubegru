de374ffde5af964438ecc88ce46254deff182143
superjojo140 <github@superjojo.de>
2022-11-18 11:30:32 +0100

    Check old calendar.js for unused code fix #82

--- modules/calendar2/calendar.js
+++ modules/calendar2/calendar.js
@@ -1,16 +1,1 @@
 CalendarController.init();
-
-
-// //Verknüpfung Raumkanal zu Mailtemplates
-// $("#calendarRoom").on("change", function () {
-//     let channel = $(this).find(':selected').attr("data-channel");
-
-//     let roomTemplateAssignment = stubegru.constants.CUSTOM_CONFIG.roomTemplateAssignment;
-//     if (roomTemplateAssignment && roomTemplateAssignment[channel]) {
-//         const templateId = roomTemplateAssignment[channel];
-//         $("#calendarTemplate").val(templateId).change();
-//     }
-// })
-
-
-
