457e514f5cfc4d0365735116bbe3e1fbb9c94297
superjojo140 <github@superjojo.de>
2023-04-17 18:10:55 +0200

    Update wikiword converter for use with pdo

--- .dev/tools/wikiword_converter.php
+++ .dev/tools/wikiword_converter.php
@@ -1,20 +1,20 @@
 <?php
 //Converts old wikiwords [article name] to new wikiwords [wikiword|12|article name]
 
-if(!isset($_GET["pwd"]) || $_GET["pwd"] != getenv("APPLICATION_ID")){
-    header("401 - No access",true,401);
-    exit;
-}
-
 $BASE_PATH = getenv("BASE_PATH");
 require_once "$BASE_PATH/utils/auth_and_database.php";
+
 $where = "`id`>='0'";
 $currentIndex = 0;
+echo "Converting wikiwords in all articles from $server.$database where $where<br>";
 
-$ergebnis = mysqli_query($mysqli, "SELECT * FROM `wiki_artikel` WHERE $where;");
-$numberOfAllArticles = $ergebnis->num_rows;
+$testStatement = $dbPdo->query("SELECT count(*) FROM `wiki_artikel` WHERE $where;");
+$numberOfAllArticles = $testStatement->fetchColumn();
 
-while ($row = mysqli_fetch_object($ergebnis)) {
+$ergebnis = $dbPdo->query("SELECT * FROM `wiki_artikel` WHERE $where;");
+$resultList = $ergebnis->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($resultList as $row) {
     //Für jeden Artikel
     $currentIndex++;
     $articleId = $row->id;
@@ -24,14 +24,23 @@ while ($row = mysqli_fetch_object($ergebnis)) {
 
     //Prüfen, ob null-wikiwords mit einer articleId verknüpft werden können
     $replace_wikiword = function ($matches) {
-        global $mysqli;
+        global $dbPdo;
         $match = $matches[0]; //Only full matches are interesting
+
+        $badMatches = array("if gte mso","endif");
+
+        foreach($badMatches as $badM)
+        if (str_contains($match, $badM)) {
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;[WARNING] Found regex match <b>$match</b> but seems to be not a wikiword. Skipping this match<br>";
+            return $match;
+        }
+
         $title = substr($match, 1, -1); //remove brackets
         //lookup DB for article with this title
-        $ergebnis = mysqli_query($mysqli, "SELECT id FROM `wiki_artikel` WHERE heading = '$title';");
-        if ($ergebnis->num_rows > 0) {
-            $row = mysqli_fetch_object($ergebnis);
-            $articleId = $row->id;
+        $headingResult = $dbPdo->query("SELECT id FROM `wiki_artikel` WHERE heading = '$title';");
+        $headingList = $headingResult->fetchAll(PDO::FETCH_OBJ);
+        foreach ($headingList as $headingRow) {
+            $articleId = $headingRow->id;
             echo "&nbsp;&nbsp;&nbsp;&nbsp;[SUCCESS] Found wikiword <b>$title</b> matching with article id <b>$articleId</b><br>";
             return "[wikiword|$articleId|$title]";
         }
@@ -41,5 +50,5 @@ while ($row = mysqli_fetch_object($ergebnis)) {
     $regex = "/\[(\w|ä|Ä|ü|Ü|ö|Ö|ß|\"|\'|\§|\/|\€|\?|\!|\-|\(|\)|\,|\.|\ )*\]/mi";
     $articleText = preg_replace_callback($regex, $replace_wikiword, $articleText);
     //aktualisierten Text in der DB speichern
-    mysqli_query($mysqli, "UPDATE `wiki_artikel` SET `text`='$articleText' WHERE `id` = '$articleId'");
+    $dbPdo->query("UPDATE `wiki_artikel` SET `text`='$articleText' WHERE `id` = '$articleId';");
 }
