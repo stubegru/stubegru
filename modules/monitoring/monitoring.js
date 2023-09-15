522cde09bf117ddae9b6ee9bf6a4b4a360f5a048
superjojo140 <github@superjojo.de>
2023-09-15 14:54:00 +0200

    Reset monitoring form after init Doesnt make sense, but fixes toggle problems...

--- modules/monitoring/monitoring.js
+++ modules/monitoring/monitoring.js
@@ -1,16 +1,23 @@
-stubegru.modules.menubar.addItem("primary", `<li class="permission-MONITORING_WRITE permission-required"><a data-toggle="modal" data-target="#monitoringModal" title="Beratungskontakt erfassen"><i class="fas fa-chart-line"></i>&nbsp;Monitoring</a></li>`, -100);
+
 
 let monitoringTemplatePath,surveyId;
 initMonitoring();
 
 async function initMonitoring() {
     monitoringTemplatePath = $(`stubegruModule[data-name="monitoring"]`).attr("data-template-path");
+
     if(!monitoringTemplatePath){
         console.warn(`[Monitoring] Could not init monitoring form. Please specify "data-template-path" attribute.`);
         return;
     }
     surveyId = await stubegru.modules.survey.initSurvey(monitoringTemplatePath, "#monitoringContainerEvaluation", "");
+
+    stubegru.modules.menubar.addItem("primary", `<li class="permission-MONITORING_WRITE permission-required"><a data-toggle="modal" data-target="#monitoringModal" title="Beratungskontakt erfassen"><i class="fas fa-chart-line"></i>&nbsp;Monitoring</a></li>`, -100);
+
     stubegru.modules.menubar.addItem("secondary", `<li class="permission-MONITORING_READ permission-required"><a title="Monitoring Ergebnisse als CSV herunterladen" href="${stubegru.constants.BASE_URL}/modules/survey/get_survey_answers.php?surveyId=${surveyId}"><i class="fas fa-download"></i>&nbsp;Download Monitoring</a></li>`, 1);
+
+    await stubegru.modules.survey.resetSurvey(monitoringTemplatePath, "#monitoringContainerEvaluation", "");
+
     stubegru.modules.customEvents.trigger("postMonitoringReset").catch(()=>{});
 }
 
