b66d6d71ed6b0f68694acb13d8e4a32782c4c0e1
superjojo140 <github@superjojo.de>
2023-10-27 10:32:55 +0200

    Move event type form to static html

--- modules/event_management/event_types/event_types.js
+++ modules/event_management/event_types/event_types.js
@@ -1,11 +1,3 @@
-const eventTypeTemplatePath = $(`stubegruModule[data-name="event_management/event_types"]`).attr("data-template-path");
-
-
-stubegru.modules.survey.initSurvey(eventTypeTemplatePath, "#surveyContainerEventType").then(() => {
-    $("#surveyContainerEventType").show();
-});
-
 function editEventType(eventTypeId){
-    stubegru.modules.survey.resetSurvey(eventTypeTemplatePath, "#surveyContainerEventType");
     $("#eventTypeModal").modal("show");
 }
