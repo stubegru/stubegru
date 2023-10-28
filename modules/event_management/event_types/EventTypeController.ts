178f6c559ccdd48f5cbd31e811cc49f39507f464
superjojo140 <github@superjojo.de>
2023-10-28 11:43:11 +0200

    Fix errors in event_types logic

--- modules/event_management/event_types/EventTypeController.ts
+++ modules/event_management/event_types/EventTypeController.ts
@@ -1,10 +1,19 @@
-class EventTypeController{
+class EventTypeController {
     static async delete(eventTypeId: string) {
         throw new Error("Method not implemented.");
     }
-    
-    static async getAll():Promise<EventTypeList> {
-        throw new Error("Method not implemented.");
-    }
 
+    static async refreshEventTypeList(): Promise<EventTypeList> {
+        try {
+            EventTypeView.eventTypeList = {}; //Reset static EventType list
+
+            let resp = await fetch(`${EventTypeView.stubegru.constants.BASE_URL}/modules/event_management/event_types/get_event_types.php`);
+            let eventTypeList: EventType[] = await resp.json();
+            for (const e of eventTypeList) { EventTypeView.eventTypeList[e.id] = e; }
+            return EventTypeView.eventTypeList;
+        } catch (error) {
+            EventTypeView.stubegru.modules.alerts.alert({ title: "Netzwerkfehler", text: `Beim Abrufen der Veranstaltungskategorien ist ein Fehler aufgetreten. <br><br> Fehler: <i>${error.message}</i>`, type: "error" });
+        }
+    }
 }
+
--- modules/event_management/event_types/EventTypeView.ts
+++ modules/event_management/event_types/EventTypeView.ts
@@ -1,41 +1,54 @@
 class EventTypeView {
 
     //@ts-expect-error
-    static stubegru = document.stubegru as StubegruObject;
+    static stubegru = window.stubegru as StubegruObject;
     static eventTypeList: EventTypeList = {};
 
     static init() {
-        this.refreshListView(); //Init event view
-        setInterval(this.refreshListView, 1000 * 60 * 15); //Refresh view every 15 minutes
+        EventTypeView.refreshListView(); //Init event view
+        setInterval(EventTypeView.refreshListView, 1000 * 60 * 15); //Refresh view every 15 minutes
 
         //Reset modal on hide
         let modal = document.getElementById("eventTypeModal");
-        modal.addEventListener("hidden.bs.modal", this.resetModalForm);
+        modal.addEventListener("hidden.bs.modal", EventTypeView.resetModalForm);
+
+        //Register new event type button
+        let newBtn = document.getElementById("eventTypeNewButton");
+        newBtn.addEventListener("click",EventTypeView.newEventType);
+    }
+
+    static newEventType() {
+        EventTypeView.resetModalForm();
+        EventTypeView.setModalVisible(true);
     }
 
     static resetModalForm() {
-        let form = document.getElementById("eventForm") as HTMLFormElement;
+        let form = document.getElementById("eventTypeModalForm") as HTMLFormElement;
         form.reset();
     }
 
 
     static editEventType(eventTypeId: string) {
 
-        let eventType = this.eventTypeList[eventTypeId];
+        let eventType = EventTypeView.eventTypeList[eventTypeId];
 
         //TODO set inputs elements with object properties...
 
-        //@ts-expect-error Show modal
-        $("#eventTypeModal").modal("show");
+        EventTypeView.setModalVisible(true);
     }
 
 
 
 
+    static setModalVisible(visible:boolean) {
+        //@ts-expect-error
+        $("#eventTypeModal").modal(visible ? "show" : "hide");
+    }
+
     static async refreshListView() {
 
-        let eventTypeList = await EventTypeController.getAll();
-        this.eventTypeList = eventTypeList;
+        let eventTypeList = await EventTypeController.refreshEventTypeList();
+        EventTypeView.eventTypeList = eventTypeList;
 
         let listElement = document.getElementById("eventTypeList") as HTMLElement;
         listElement.innerHTML = "";
@@ -52,7 +65,7 @@ class EventTypeView {
         let deleteBtnList = document.querySelectorAll(".event-type-delete-button");
         for (const btn of deleteBtnList) {
             let eventTypeId = btn.getAttribute("data-event-type-id");
-            this.stubegru.modules.alerts.deleteConfirm("Abwesenheit löschen", "Soll diese Abwesenheit wirklich gelöscht werden?", async () => await EventTypeController.delete(eventTypeId));
+            EventTypeView.stubegru.modules.alerts.deleteConfirm("Abwesenheit löschen", "Soll diese Abwesenheit wirklich gelöscht werden?", async () => await EventTypeController.delete(eventTypeId));
         }
 
 
@@ -60,10 +73,12 @@ class EventTypeView {
         let editBtnList = document.querySelectorAll(".event-type-edit-button");
         for (const btn of editBtnList) {
             let eventTypeId = btn.getAttribute("data-event-type-id");
-            this.editEventType(eventTypeId);
+            EventTypeView.editEventType(eventTypeId);
         }
 
-        this.stubegru.modules.userUtils.updateAdminElements()
+        EventTypeView.stubegru.modules.userUtils.updateAdminElements()
     }
 
 }
+
+EventTypeView.init();
