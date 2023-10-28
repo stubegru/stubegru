8a0c7a06dd56071f305dbb53a948e9f6130868f8
superjojo140 <github@superjojo.de>
2023-10-28 11:06:08 +0200

    Move deleteConfirm to sweetalert.js

--- modules/alerts/alerts.js
+++ modules/alerts/alerts.js
@@ -32,6 +32,28 @@ stubegru.modules.alerts.alert = function (options,title) {
     else { throw new Error(`[Alerts] Unknown alert mode '${options.mode}'. Can't trigger alert.`) }
 }
 
+function deleteConfirm(pTitle, pDescription, callback) { //Dialog der vor dem Löschen von Einträgen aufgerufen wird. nachricht mit pTitle und pDescription wird angezeigt. falls auf "Löschen" geklickt wird, wird die übergebene Funktion "callback" aufgerufen. Dies ist in der Regel dann die Funktion, die das Element aus der DB löscht.
+    swal({
+        title: pTitle,
+        text: pDescription,
+        type: "error",
+        showCancelButton: true,
+        cancelButtonText: "Abbrechen",
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Löschen",
+    }, function () {
+        callback();
+        /*stubegru.modules.alerts.alert({
+            title: "Gelöscht!",
+            text: "Der Eintrag wurde erfolgreich gelöscht",
+            type: "success",
+            timer: 2000
+        });*/
+    });
+}
+
+stubegru.modules.alerts.deleteConfirm = deleteConfirm;
+
 function showToast(options) {
     let html = `<div class="alert alert-${options.type} alert-dismissible" role="alert" style="display:none;">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
