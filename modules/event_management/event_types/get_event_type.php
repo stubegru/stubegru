7f3f41a937cd5cc85ef5315d7c5c0c418787c8fd
superjojo140 <github@superjojo.de>
2023-11-13 15:26:39 +0100

    Update get_event_type.php

--- modules/event_management/event_types/get_event_type.php
+++ modules/event_management/event_types/get_event_type.php
@@ -8,13 +8,17 @@ try {
     if (isset($_GET["eventTypeId"]) && is_numeric($_GET["eventTypeId"])) {
         $eventTypeId = $_GET["eventTypeId"];
     } else {
-        throw new Exception("Veranstaltungskategorie mit der id '$eventTypeId' konnte nicht abgerufen werden.", 404);
+        throw new Exception("Die Id '" . $_GET['eventTypeId'] . "' ist keine gültige Id für eine Veranstaltungskategorie.", 404);
     }
 
     $selectStatement = $dbPdo->prepare("SELECT * FROM `event_mgmt_types` WHERE eventTypeId = :eventTypeId;");
     $selectStatement->bindValue(':eventTypeId', $eventTypeId);
     $selectStatement->execute();
     $attributeRowList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($attributeRowList) <= 0) {
+        throw new Exception("Veranstaltungskategorie mit der id '$eventTypeId' wurde nicht gefunden.", 404);
+    }
 
     $eventType = array();
     $eventType["id"] = $eventTypeId;
@@ -36,6 +40,6 @@ try {
     }
     echo json_encode($eventType);
 } catch (\Throwable $th) {
-    echo json_encode(array("status" => "error", "message" => "Veranstaltung mit der id $eventTypeId konnte nicht abgerufen werden."));
+    echo json_encode(array("status" => "error", "message" => $th->getMessage()));
     exit;
 }
