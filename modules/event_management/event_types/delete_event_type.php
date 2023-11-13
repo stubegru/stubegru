f99bfbcde512256295d7d2fd4a983c5bd4cae0f1
superjojo140 <github@superjojo.de>
2023-11-13 16:13:36 +0100

    Add delete_event_type.php

---
+++ modules/event_management/event_types/delete_event_type.php
@@ -0,0 +1,24 @@
+<?php
+
+try {
+    $BASE_PATH = getenv("BASE_PATH");
+    require_once "$BASE_PATH/utils/auth_and_database.php";
+    permissionRequest("EVENT_TYPE_WRITE");
+    $ownId = $_SESSION["id"];
+
+    if (isset($_POST["eventTypeId"]) && is_numeric($_POST["eventTypeId"])) {
+        $eventTypeId = $_POST["eventTypeId"];
+    } else {
+        throw new Exception("Die Id '" . $_POST['eventTypeId'] . "' ist keine gültige Id für eine Veranstaltungskategorie.", 404);
+    }
+
+    $selectStatement = $dbPdo->prepare("DELETE * FROM `event_mgmt_types` WHERE eventTypeId = :eventTypeId;");
+    $selectStatement->bindValue(':eventTypeId', $eventTypeId);
+    $selectStatement->execute();
+
+    echo json_encode(array("status" => "success", "message" => "Veranstaltungskategorie mit der id '$eventTypeId' erfolgreich gelöscht."));
+
+} catch (\Throwable $th) {
+    echo json_encode(array("status" => "error", "message" => $th->getMessage()));
+    exit;
+}
