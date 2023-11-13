a30f40dddc4f6c62f07f6fbefdfd7733d9bd20df
superjojo140 <github@superjojo.de>
2023-11-13 15:39:27 +0100

    Add get_all_event_types.php

---
+++ modules/event_management/event_types/get_all_event_types.php
@@ -0,0 +1,39 @@
+<?php
+try {
+    $BASE_PATH = getenv("BASE_PATH");
+    require_once "$BASE_PATH/utils/auth_and_database.php";
+    permissionRequest("EVENT_TYPE_READ");
+    $ownId = $_SESSION["id"];
+
+    $selectStatement = $dbPdo->query("SELECT * FROM `event_mgmt_types`;");
+    $attributeRowList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+
+    $eventTypeList = array();
+
+    foreach ($attributeRowList as $attributeRow) {
+        $eventTypeId = $attributeRow["eventTypeId"];
+        $attributeId = $attributeRow["attributeId"];
+        $attributeValue = $attributeRow["value"];
+        $isMultiple = $attributeRow["multiple"];
+
+        if (empty($eventTypeList[$eventTypeId])) {
+            $eventTypeList[$eventTypeId] = array(); //Create new event type "object"
+            $eventTypeList[$eventTypeId]["id"] = $eventTypeId;
+        }
+        $eventType = &$eventTypeList[$eventTypeId]; 
+        
+        //Add current value as property to eventType Object. If this property is multiple -> create a list of properties at that point.
+        if ($isMultiple) {
+            if (empty($eventType[$attributeId])) {
+                $eventType[$attributeId] = array(); //Create new list of values 
+            }
+            $eventType[$attributeId][] = $attributeValue; //Add new value to list of values
+        } else {
+            $eventType[$attributeId] = $attributeValue; //Add value as plain property
+        }
+    }
+    echo json_encode($eventTypeList);
+} catch (\Throwable $th) {
+    echo json_encode(array("status" => "error", "message" => $th->getMessage()));
+    exit;
+}
