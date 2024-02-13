687b379740a3a19a972a16f1247355ddf1f146aa
superjojo140 <github@superjojo.de>
2024-02-13 10:26:42 +0100

    Add portfolio script (not working yet)

---
+++ modules/event_management/portfolio/get_portfolio_event_instances.php
@@ -0,0 +1,37 @@
+<?php
+try {
+    $BASE_PATH = getenv("BASE_PATH");
+    require_once "$BASE_PATH/utils/database_without_auth.php";
+
+    $selectStatement = $dbPdo->query("SELECT * FROM `event_mgmt_instances`;");
+    $attributeRowList = $selectStatement->fetchAll(PDO::FETCH_ASSOC);
+
+    $eventInstanceList = array();
+
+    foreach ($attributeRowList as $attributeRow) {
+        $eventInstanceId = $attributeRow["eventInstanceId"];
+        $attributeKey = $attributeRow["attributeKey"];
+        $attributeValue = $attributeRow["value"];
+        $isMultiple = $attributeRow["multiple"];
+
+        if (empty($eventInstanceList[$eventInstanceId])) {
+            $eventInstanceList[$eventInstanceId] = array(); //Create new event instance "object"
+            $eventInstanceList[$eventInstanceId]["id"] = $eventInstanceId;
+        }
+        $eventInstance = &$eventInstanceList[$eventInstanceId]; 
+        
+        //Add current value as property to eventInstance Object. If this property is multiple -> create a list of properties at that point.
+        if ($isMultiple) {
+            if (empty($eventInstance[$attributeKey])) {
+                $eventInstance[$attributeKey] = array(); //Create new list of values 
+            }
+            $eventInstance[$attributeKey][] = $attributeValue; //Add new value to list of values
+        } else {
+            $eventInstance[$attributeKey] = $attributeValue; //Add value as plain property
+        }
+    }
+    echo json_encode($eventInstanceList);
+} catch (\Throwable $th) {
+    echo json_encode(array("status" => "error", "message" => $th->getMessage()));
+    exit;
+}
