1951da5bef645949c7a0cd4225615cfb31db4640
superjojo140 <github@superjojo.de>
2024-02-16 11:45:00 +0100

    build basic portfolio backend

--- modules/event_management/portfolio/portfolio.js
+++ modules/event_management/portfolio/portfolio.js
@@ -11,18 +11,30 @@ async function renderEventInstances(eventInstanceList) {
         let e = new EventInstance();
         e = eventInstanceList[eventId];
 
+        let assigneeHtml = "";
+        if (e.assigneesInternal) {
+            for (const assignee of e.assigneesInternal) {
+                assigneeHtml += `
+                <div class="alert alert-success">
+                    Name: ${assignee.name} <br>
+                    Mail: ${assignee.mail} <br>
+                </div>`
+            }
+        }
+
         html += `
             <div class="panel panel-default">
               <div class="panel-heading">
                 <h3 class="panel-title">${e.name}</h3>
               </div>
               <div class="panel-body">
-                Kategorie: ${e.category}<br>
+                Kategorie: ${e.category.name}<br>
                 Datum: ${e.startDate} bis ${e.endDate}<br>
                 Uhrzeit: ${e.startTime} bis ${e.endTime}<br>
                 Ort: ${e.location}<br>
+                Verantwortlich: ${assigneeHtml}<br>
                 <br>
-                ${e.announcementPR}
+                ${e.category.descriptionInternal}
               </div>
             </div>
             `;
