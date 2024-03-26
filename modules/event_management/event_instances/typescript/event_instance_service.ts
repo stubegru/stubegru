907be190a5e6a465e247ecdeca49a21f969c7d4b
superjojo140 <github@superjojo.de>
2024-03-26 11:02:24 +0100

    handle eventInstance cancel

--- modules/event_management/event_instances/typescript/event_instance_service.ts
+++ modules/event_management/event_instances/typescript/event_instance_service.ts
@@ -1,5 +1,12 @@
 class EventInstanceService {
 
+    static async cancel(eventInstanceId: string) {
+        let resp = await fetch(`${EventInstanceController.stubegru.constants.BASE_URL}/modules/event_management/event_instances/backend/cancel_event_instance.php?eventInstanceId=${eventInstanceId}`);
+        let parsedResp: StubegruHttpResponse = await resp.json();
+        if (!parsedResp.status || parsedResp.status == "error") { throw new Error(parsedResp.message); }
+        return parsedResp;
+    }
+
     static async getAll(): Promise<StringIndexedList<EventInstance>> {
         let resp = await fetch(`${EventInstanceController.stubegru.constants.BASE_URL}/modules/event_management/event_instances/backend/get_all_event_instances.php`);
         let eventInstanceList: StringIndexedList<EventInstance> = await resp.json();
