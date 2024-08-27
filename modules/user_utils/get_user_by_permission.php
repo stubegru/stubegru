5764a732e5621f3c168b1313307dc54b173bbadb
superjojo140 <github@superjojo.de>
2024-08-27 15:21:03 +0200

    order userlist by user.name

--- modules/user_utils/get_user_by_permission.php
+++ modules/user_utils/get_user_by_permission.php
@@ -11,24 +11,25 @@ $selectStatement->bindValue(':permissionRequest', $permissionRequest);
 $selectStatement->execute();
 $permissionList = $selectStatement->fetchAll(PDO::FETCH_COLUMN);
 
-if(count($permissionList) <= 0){
+if (count($permissionList) <= 0) {
     echo json_encode(array("status" => "error", "message" => "No permissionRequest found with name $permissionRequest"));
     exit;
 }
 
 $complexQuery = "SELECT Nutzer.name, Nutzer.id, Nutzer.mail, Nutzer.account FROM Nutzer WHERE";
 //build "AND-statement" for each required permission
-foreach($permissionList as $index => $permission){
-    if($index > 0){
+foreach ($permissionList as $index => $permission) {
+    if ($index > 0) {
         $complexQuery .= " AND";
     }
     $complexQuery .= " Nutzer.id IN (SELECT permissions_user.userId FROM permissions_user WHERE permissions_user.permissionId = :perm$index)";
 }
+$complexQuery .= " ORDER BY Nutzer.name;";
 
 //prepare and bind values
 $complexSelect = $dbPdo->prepare($complexQuery);
-foreach($permissionList as $index => $permission){
-    $complexSelect->bindValue(":perm$index",$permission);
+foreach ($permissionList as $index => $permission) {
+    $complexSelect->bindValue(":perm$index", $permission);
 }
 
 //execute statement
