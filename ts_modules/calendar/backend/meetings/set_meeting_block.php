e8925d10dfcbf0ff60d32baf8a1af180fe481905
superjojo140 <github@superjojo.de>
2025-11-21 10:50:57 +0100

    refactor meeting_block php scripts for new meeting_blocks table

--- ts_modules/calendar/backend/meetings/set_meeting_block.php
+++ ts_modules/calendar/backend/meetings/set_meeting_block.php
@@ -6,32 +6,52 @@ $own_id = $_SESSION['id'];
 
 try {
     $meetingId = $_POST["meetingId"];
-    $blockMeeting = $_POST["blockMeeting"] == 1 ? $own_id : 0;
+    $blockMeeting = $_POST["blockMeeting"] == 1;
 
-    $selectStatement = $dbPdo->prepare("SELECT blocked FROM `Termine` WHERE id = :meetingId;");
+    //delete expired blocks
+    $dbPdo->query("DELETE FROM `meeting_blocks` WHERE `timestamp` < (NOW() - INTERVAL 30 MINUTE);");
+
+    //check for existing block
+    $selectStatement = $dbPdo->prepare("SELECT * FROM `meeting_blocks` WHERE meetingId = :meetingId;");
     $selectStatement->bindValue(':meetingId', $meetingId);
     $selectStatement->execute();
-    $alreadyBlocked = $selectStatement->fetchColumn();
-
-
-    if ($blockMeeting != 0 && ($alreadyBlocked != 0 && $alreadyBlocked != $own_id)) {
-        echo json_encode(array("status" => "error", "message" => "Der Termin ist bereits blockiert und konnte nicht erneut blockiert werden.", "blockId" => $alreadyBlocked));
-        exit;
+    $blockedRow = $selectStatement->fetch(PDO::FETCH_ASSOC);
+    $alreadyBlocked = false;
+    if ($blockedRow && isset($blockedRow['meetingId'])) {
+        $alreadyBlocked = true;
+        $blockUserId = $blockedRow['userId'];
     }
 
-    if ($blockMeeting == 0 && ($alreadyBlocked != $own_id && $alreadyBlocked != 0)) {
-        echo json_encode(array("status" => "error", "message" => "Der Termin ist durch eine andere Person blockiert. Die Blockierung kann nicht aufgehoben werden.", "blockId" => $alreadyBlocked));
-        exit;
-    }
+    if ($blockMeeting) {
+        //meeting should be blocked AND is already blocked BY another person => ERROR
+        if ($alreadyBlocked == true && $blockUserId != $own_id) {
+            echo json_encode(array("status" => "error", "message" => "Der Termin ist bereits blockiert und konnte nicht erneut blockiert werden.", "blockId" => $blockUserId));
+            exit;
+        }
 
-    $updateStatement = $dbPdo->prepare("UPDATE `Termine` SET blocked = :blockMeeting WHERE id = :meetingId");
-    $updateStatement->bindValue(':blockMeeting', $blockMeeting);
-    $updateStatement->bindValue(':meetingId', $meetingId);
-    $updateStatement->execute();
+        // Insert new block
+        $insertStatement = $dbPdo->prepare("INSERT INTO `meeting_blocks` (`meetingId`, `userId`, `timestamp`) VALUES (:meetingId, :userId, NOW())");
+        $insertStatement->bindValue(':meetingId', $meetingId);
+        $insertStatement->bindValue(':userId', $own_id);
+        $insertStatement->execute();
+        echo json_encode(array("status" => "success", "message" => "Termin wurde erfolgreich blockiert"));
+
+    } else {
+        //meeting should be UN-blocked AND is already blocked BY another person => ERROR
+        if ($alreadyBlocked == true && $blockUserId != $own_id) {
+            echo json_encode(array("status" => "error", "message" => "Der Termin ist durch eine andere Person blockiert. Die Blockierung kann nicht aufgehoben werden.", "blockId" => $blockUserId));
+            exit;
+        }
+
+        // Remove block
+        $deleteStatement = $dbPdo->prepare("DELETE FROM `meeting_blocks` WHERE `meetingId` = :meetingId");
+        $deleteStatement->bindValue(':meetingId', $meetingId);
+        $deleteStatement->execute();
+        echo json_encode(array("status" => "success", "message" => "Termin wurde erfolgreich freigegeben"));
+    }
 } catch (Exception $e) {
     echo json_encode(array("status" => "error", "message" => "Der Termin konnte nicht blockiert werden."));
     exit;
 }
 
 
-echo json_encode(array("status" => "success", "message" => "Blockieren / Block aufheben erfolgreich"));
