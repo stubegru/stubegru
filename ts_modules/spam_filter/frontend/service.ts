9a0ec39d3cfb3745a65844ed0465f64eb4501741
superjojo140 <github@superjojo.de>
2025-12-10 10:49:40 +0100

    Update spam filter backend files for stubegru usage

--- ts_modules/spam_filter/frontend/service.ts
+++ ts_modules/spam_filter/frontend/service.ts
@@ -5,12 +5,12 @@ import { SpamFilter } from "./model.js";
 export default class SpamFilterService {
 
     async getAllSpamFilter() {
-        const resp = await Stubegru.fetch.getJson("ts_modules/spam_filter/backend/get_spam_filter.php") as SpamFilter[];
+        const resp = await Stubegru.fetch.getJson("ts_modules/spam_filter/backend/get_all_spam_filter.php") as SpamFilter[];
         return resp;
     }
 
     async getSpamFilter(spamFilterId: string) {
-        const resp = await Stubegru.fetch.getJson("ts_modules/spam_filter/backend/get_spam_filter.php", { spamFilterId: spamFilterId }) as SpamFilter;
+        const resp = await Stubegru.fetch.getJson("ts_modules/spam_filter/backend/get_spam_filter.php", { id: spamFilterId }) as SpamFilter;
         return resp;
     }
 
@@ -27,7 +27,7 @@ export default class SpamFilterService {
     }
 
     async deleteSpamFilter(spamFilterId: string) {
-        let resp = await Stubegru.fetch.postJson("ts_modules/spam_filter/backend/delete_spam_filter.php", { spamFilterId: spamFilterId }) as StubegruHttpResponse;
+        let resp = await Stubegru.fetch.postJson("ts_modules/spam_filter/backend/delete_spam_filter.php", { id: spamFilterId }) as StubegruHttpResponse;
         if (resp.status == "error") { throw new Error(resp.message) };
         return resp;
     }
